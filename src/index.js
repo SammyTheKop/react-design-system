c81d4eea5deb1df719f3c080e2e68b502eac7018
Samrat Ghosh <samrat@hellotars.com>
2020-04-24 21:43:33 +0530

    Added the index file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// react components
+import Button from "./components/button/button";
+import Card from "./components/card/card";
+import Dropdown from "./components/dropdown/dropdown";
+import IconButton from "./components/iconButton/iconButton";
+import Label from "./components/label/label";
+import Modal from "./components/modal/modal";
+import Platter from "./components/platter/platter";
+import Textbox from "./components/textbox/textbox";
+
+// Styles
+import * as styles from "./shared/styles";
+import * as global from "./shared/global";
+import * as animation from "./shared/animation";
+import * as icons from "./shared/icons";
+
+// exporting styles
+export { styles, global, animation, icons };
+
+// exporting components
+export { Button, Card, Dropdown, IconButton, Label, Modal, Textbox, Platter };
